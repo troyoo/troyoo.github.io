23e08506be97e0228e1a24b4a2828be47d5cc61c
troyoo <troyoo@163.com>
2014-08-05 14:07:33 +0800

    Create zqgg.js

---
+++ javascripts/zqgg.js
@@ -0,0 +1,1 @@
+document.write("<a><img src='http://d.hiphotos.bdimg.com/album/s%3D1000%3Bq%3D90/sign=432353f332adcbef05347a069c9f15ac/4afbfbedab64034feb71a831acc379310a551d4c.jpg' border='0'></a>);
